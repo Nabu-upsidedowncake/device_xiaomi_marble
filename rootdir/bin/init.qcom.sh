819ee98468d68f6f7c3851b0c4ea22cc26a3a82c
Giovix92 <ggualtierone@gmail.com>
2019-05-28 16:29:45 +0200

    marble: rootdir: Clear package_cache after dirty flash
     * This helps after a dirty flash, in order to avoid broken strings on settings app
    
    Signed-off-by: harshhaareddy <harshhaa03@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: RobertGarciaa <chae0218@naver.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- rootdir/bin/init.qcom.sh
+++ rootdir/bin/init.qcom.sh
@@ -526,3 +526,6 @@ chmod 660 /config/stp-policy/coresight-stm:p_ost.policy
 mkdir /config/stp-policy/coresight-stm:p_ost.policy/default
 chmod 660 /config/stp-policy/coresight-stm:p_ost.policy/default
 echo 0x10 > /sys/bus/coresight/devices/coresight-stm/traceid
+
+# Clear packages_cache to avoid systemapp have mismatch strings id
+rm -rf /data/system/package_cache
