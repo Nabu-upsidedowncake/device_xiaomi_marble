4c15cdf1eba2ab66bac1e08feb77a2cea508412c
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-05-05 19:34:08 +0800

    marble: Disable kpti
    It can also help increase performance by a lot in some scenarios.
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Change-Id: Ica465f586a44129047c266597316045fab762550
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,6 +107,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     disable_dma32=on \
+    kpti=off \
     swinfo.fingerprint=$(AWAKEN_VERSION) \
     mtdoops.fingerprint=$(AWAKEN_VERSION)
 
