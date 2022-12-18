edf9b12f51460f6d813d94784d3cbecea30873d4
Alex Damaratski <alexeydomoratsky1@gmail.com>
2022-12-18 21:58:05 +0000

    marble: Build `libsuspend`
    * Required by kpoc_charger
    
    Change-Id: Ia33222329585abc451d9490c40728e7ab4287298
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- device.mk
+++ device.mk
@@ -164,6 +164,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.capabilityconfigstore@1.0.vendor
 
+# Charger
+PRODUCT_PACKAGES += \
+    libsuspend
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@3.0-impl \
