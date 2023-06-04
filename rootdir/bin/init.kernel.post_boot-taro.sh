a1312287b78112141e16f35c5e9cb18ff7c08480
Jake Weinstein <jake@aospa.co>
2023-06-04 05:41:09 +0300

    marble: rootdir: Move background cpuset to CPU0-1
    Background tasks are not UX-critical.
    Free up cores to work on more important
    UX tasks.

--- rootdir/bin/init.kernel.post_boot-taro.sh
+++ rootdir/bin/init.kernel.post_boot-taro.sh
@@ -86,7 +86,7 @@ echo 0 > /proc/sys/kernel/sched_util_clamp_min_rt_default
 echo 325 > /proc/sys/walt/walt_low_latency_task_threshold
 
 # cpuset parameters
-echo 0-3 > /dev/cpuset/background/cpus
+echo 0-1 > /dev/cpuset/background/cpus
 echo 0-3 > /dev/cpuset/restricted/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
 echo 0-6 > /dev/cpuset/foreground/cpus
