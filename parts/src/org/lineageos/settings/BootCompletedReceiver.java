722fc64864fc594faffcf11b367c5b82683846df
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-10 10:15:25 -0300

    marble: parts: Introduce Dolby Atmos
    Based on existing dirac implementation and observing stock
    sound effects app and daxservice.
    
    Thanks to jhenrique09 for the hack from old dirac parts
    "Pause/play music stream to get effects applied".
    
    TODO: bring back misound (same as stock)
    
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Change-Id: Ia137963efee2f54a74b391dc5ea5016fc60a8fab
    Signed-off-by: Mesquita <mesquita@aospa.co>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -1,18 +1,9 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
  *               2017-2020 The LineageOS Project
+ * Copyright (C) 2023 Paranoid Android
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package org.lineageos.settings;
@@ -23,6 +14,7 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.dolby.DolbyUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -33,5 +25,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         ThermalUtils.startService(context);
+        DolbyUtils.getInstance(context).onBootCompleted();
     }
 }
