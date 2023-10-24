7320bffed93c976b743616c4292a6052d6eb561a
saikiran2001 <bjsaikiran@gmail.com>
2023-10-24 20:27:33 +0200

    marble: Update init.kernel.post_boot-taro.sh from CLO

--- rootdir/bin/init.kernel.post_boot-taro.sh
+++ rootdir/bin/init.kernel.post_boot-taro.sh
@@ -76,21 +76,13 @@ echo 5000000 5000000 5000000 5000000 5000000 5000000 5000000 2000000 > /proc/sys
 echo 255 > /proc/sys/walt/sched_util_busy_hysteresis_enable_cpus
 echo 15 15 15 15 15 15 15 15 > /proc/sys/walt/sched_util_busy_hyst_cpu_util
 
-# XM power profiling
-echo 0 > /sys/devices/system/cpu/cpufreq/policy0/walt/target_load_thresh
-echo 4 > /sys/devices/system/cpu/cpufreq/policy0/walt/target_load_shift
-echo 0 > /proc/sys/kernel/sched_util_clamp_min_rt_default
-
 # set the threshold for low latency task boost feature which prioritize
 # binder activity tasks
 echo 325 > /proc/sys/walt/walt_low_latency_task_threshold
 
 # cpuset parameters
-echo 0-2 > /dev/cpuset/background/cpus
+echo 0-3 > /dev/cpuset/background/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
-echo 4-7 > /dev/cpuset/foreground/boost/cpus
-echo 0-2,4-7 > /dev/cpuset/foreground/cpus
-echo 0-7 > /dev/cpuset/top-app/cpus
 
 # Turn off scheduler boost at the end
 echo 0 > /proc/sys/walt/sched_boost
@@ -102,11 +94,10 @@ echo 0 > /proc/sys/kernel/sched_util_clamp_min_rt_default
 echo "walt" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
 echo 0 > /sys/devices/system/cpu/cpufreq/policy0/walt/down_rate_limit_us
 echo 0 > /sys/devices/system/cpu/cpufreq/policy0/walt/up_rate_limit_us
-echo 800000 > /sys/devices/system/cpu/cpufreq/policy0/walt/rtg_boost_freq
 if [ $rev == "1.0" ]; then
 	echo 1190400 > /sys/devices/system/cpu/cpufreq/policy0/walt/hispeed_freq
 else
-	echo 998000 > /sys/devices/system/cpu/cpufreq/policy0/walt/hispeed_freq
+	echo 1267200 > /sys/devices/system/cpu/cpufreq/policy0/walt/hispeed_freq
 fi
 echo 614400 > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
 echo 1 > /sys/devices/system/cpu/cpufreq/policy0/walt/pl
@@ -119,18 +110,14 @@ else
 fi
 echo 100 > /proc/sys/walt/input_boost/input_boost_ms
 
-echo 1785600 0 0 0 2169600 0 0 2419200 > /proc/sys/walt/input_boost/powerkey_input_boost_freq
-echo 120 > /proc/sys/walt/input_boost/powerkey_input_boost_ms
-
 # configure governor settings for gold cluster
 echo "walt" > /sys/devices/system/cpu/cpufreq/policy4/scaling_governor
 echo 0 > /sys/devices/system/cpu/cpufreq/policy4/walt/down_rate_limit_us
 echo 0 > /sys/devices/system/cpu/cpufreq/policy4/walt/up_rate_limit_us
-echo 600000 > /sys/devices/system/cpu/cpufreq/policy4/walt/rtg_boost_freq
 if [ $rev == "1.0" ]; then
 	echo 1497600 > /sys/devices/system/cpu/cpufreq/policy4/walt/hispeed_freq
 else
-	echo 1497600 > /sys/devices/system/cpu/cpufreq/policy4/walt/hispeed_freq
+	echo 1555200 > /sys/devices/system/cpu/cpufreq/policy4/walt/hispeed_freq
 fi
 echo 1 > /sys/devices/system/cpu/cpufreq/policy4/walt/pl
 
@@ -141,7 +128,7 @@ echo 0 > /sys/devices/system/cpu/cpufreq/policy7/walt/up_rate_limit_us
 if [ $rev == "1.0" ]; then
 	echo 1536000 > /sys/devices/system/cpu/cpufreq/policy7/walt/hispeed_freq
 else
-	echo 1536000 > /sys/devices/system/cpu/cpufreq/policy7/walt/hispeed_freq
+	echo 1728000 > /sys/devices/system/cpu/cpufreq/policy7/walt/hispeed_freq
 fi
 echo 1 > /sys/devices/system/cpu/cpufreq/policy7/walt/pl
 
