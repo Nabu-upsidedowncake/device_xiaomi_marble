25e5146c31afed478f20d01d0ce75c819c5783e8
saikiran2001 <bjsaikiran@gmail.com>
2023-11-03 18:57:21 +0100

    marble: Adapt sepolicy path
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -212,7 +212,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr/sm8450/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
