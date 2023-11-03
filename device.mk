265814070c232883631fc4f22910f5a56ead9882
Master <mhmdeveloperofficial@gmail.com>
2023-11-03 04:49:21 +0000

    marble: Add ViPER4Android FX support
    * Need to clone this:
    git clone https://github.com/swiitch-OFF-Lab/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- device.mk
+++ device.mk
@@ -630,6 +630,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# Viper
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
