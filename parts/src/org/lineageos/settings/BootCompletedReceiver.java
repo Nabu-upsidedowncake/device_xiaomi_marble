b2f7d948d53cae3eb302d18e73b055bc136f0e15
saikiran2001 <bjsaikiran@gmail.com>
2023-11-14 04:40:41 +0000

    [SQUASH] marble: Import dirac, clear speaker, doze service from sm8250-common tree
    Co-authored-by: TheScarastic <warabhishek@gmail.com>
    Co-authored-by: jhenrique09 <jhsv09@gmail.com>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: imjyotiraditya <jyotiraditya@aospa.co>
    Co-authored-by: wight554 <wight554@gmail.com>
    Co-authored-by: nullxception <enuma.alrizky@gmail.com>
    Co-authored-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>
    Co-authored-by: imjyotiraditya <devpandaxda@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -11,9 +11,18 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.Log;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
+import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.camera.NfcCameraService;
+import org.lineageos.settings.display.ColorService;
+import org.lineageos.settings.dirac.DiracUtils;
+import org.lineageos.settings.doze.AodBrightnessService;
+import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.doze.PocketService;
 import org.lineageos.settings.display.ColorService;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
@@ -25,14 +34,31 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         ThermalUtils.startService(context);
+
+         // Dirac
+         try {
+            DiracUtils.getInstance(context);
+         } catch (Exception e) {
+            Log.d(TAG, "Dirac is not present in system");
+         }
+
+        // Doze
+        DozeUtils.checkDozeService(context);
+
+        // Pocket
+        PocketService.startService(context);
 
         // DisplayFeature
         ColorService.startService(context);
 
         // NFC
         NfcCameraService.startService(context);
+
+        // AOD
+        AodBrightnessService.startService(context);
 
         // Per app refresh rate
         RefreshUtils.startService(context);
