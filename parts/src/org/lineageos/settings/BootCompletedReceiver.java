0fac8bca2c732c6e678b3f1edca2e87661383244
saikiran2001 <bjsaikiran@gmail.com>
2023-11-28 06:33:02 +0000

    marble: Kill dirac with fire

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -19,7 +19,6 @@ import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.camera.NfcCameraService;
 import org.lineageos.settings.display.ColorService;
-import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.dolby.DolbyUtils;
 import org.lineageos.settings.doze.AodBrightnessService;
 import org.lineageos.settings.doze.DozeUtils;
@@ -38,13 +37,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         ThermalUtils.startService(context);
-
-         // Dirac
-         // try {
-         //   DiracUtils.getInstance(context);
-         // } catch (Exception e) {
-         //   Log.d(TAG, "Dirac is not present in system");
-         // }
 
         // Dolby Atmos
         DolbyUtils.getInstance(context).onBootCompleted();
