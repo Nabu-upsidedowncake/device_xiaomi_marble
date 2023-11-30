56de3da0567aa5edbd443b6d7f84d395e887be18
saikiran2001 <bjsaikiran@gmail.com>
2023-11-30 06:59:45 +0000

    marble: Move media_codecs_dolby_audio.xml to device tree
    * Also disable AC4 audio codec as it is not supported by AOSP

--- device.mk
+++ device.mk
@@ -364,6 +364,9 @@ PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/codec2/service/1.0/c2audio.vendor.ext-arm64.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy \
     $(AUDIO_HAL_DIR)/configs/common/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
+
 # OMX
 PRODUCT_PACKAGES += \
     libOmxCore \
