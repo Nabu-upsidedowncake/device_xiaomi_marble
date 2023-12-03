bd2ebac7ca4db76d3a8371eaf15eadff5669d837
saikiran2001 <bjsaikiran@gmail.com>
2023-12-03 19:12:59 +0000

    marble: Build vendor.qti.hardware.wifi.hostapd@1.2.vendor & vendor.qti.hardware.wifi.supplicant@2.2.vendor

--- device.mk
+++ device.mk
@@ -651,6 +651,8 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
+    vendor.qti.hardware.wifi.hostapd@1.2.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.2.vendor \
     wpa_cli \
     wpa_supplicant \
     wpa_supplicant.conf
